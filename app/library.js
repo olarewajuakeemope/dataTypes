eaf4140f8f7c2549cad1cdf1ceb633d3ead16ab7
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-03-11 23:09:36 +0100

    modified library.js to accommodate null or undefined input

--- app/library.js
+++ app/library.js
@@ -5,6 +5,8 @@ module.exports = {
  dataTypes: function(input_arg) {
    if(typeof input_arg==='string'){
 	   return input_arg.length;
+   }else if(input_arg==undefined || input_arg==null){
+       return 'no value';
    }
  }
 }
